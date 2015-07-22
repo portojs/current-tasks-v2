5e0ebd9ba26e01029425c3088efec5d47ce27ab7
peter <peter.k.nets@gmail.com>
2015-07-22 13:29:36 +0300

    Add slide-in-out function to Task Lists

--- js/app.js
+++ js/app.js
@@ -2,7 +2,16 @@
  * Created by Peter on 22.07.2015.
  */
 function TaskListLogic() {
-    $(task)
+    this.activateTaskList = function() {
+        $(this).toggleClass("task_list_active");
+        if ($(this).hasClass("task_list_active")) {
+            $(this).animate({"left": "0"}, "fast");
+        }
+        else {
+            $(this).animate({"left": "-450px"}, "fast");
+        }
+    };
+    $(".main_body").on("click.show_task_list", ".task_list", this.activateTaskList)
 }
 
 $(document).ready(function(){
