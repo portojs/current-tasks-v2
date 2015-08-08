a62c29dbb75ef1f319399cfeda74754420064e80
peter <peter.k.nets@gmail.com>
2015-08-08 10:24:24 +0300

    Mouseover function done with CSS hover feature

--- js/app.js
+++ js/app.js
@@ -11,26 +11,7 @@ function TaskListLogic() {
             $(this).animate({"left": "-450px"}, "fast");
         }
     };
-    this.tasksHover = function() {
-        $(this).addClass("task_list_hover");
-        if ($(this).hasClass("task_list_hover")) {
-            alert("Hover class attached");
-        }
-        else {
-            alert("Hover class not attached");
-        }
-        /*
-        $(this).toggleClass("task_list_hover");
-        if ($(this).hasClass("task_list_hover")) {
-            $(this).css("background-color", "lightgoldenrodyellow");
-        }
-        else {
-            $(this).css("background-color", "lightgrey");
-        }
-        */
-    };
     $(".main_body").on("click.show_task_list", ".task_list", this.activateTaskList);
-    $(".task_list").on("mouseover", "li", this.tasksHover);
 }
 
 $(document).ready(function(){
