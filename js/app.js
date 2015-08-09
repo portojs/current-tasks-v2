b47f442fc246052b08234120f7ce32a94c3b1c9e
peter <peter.k.nets@gmail.com>
2015-08-09 11:49:25 +0300

    Prevent propagation when input field is clicked

--- js/app.js
+++ js/app.js
@@ -15,8 +15,12 @@ function TaskListLogic() {
         event.stopPropagation();
         $(this).closest(".task_list").find(".add_task_form").slideToggle();
     };
+    this.preventPropagation = function(event) {
+        event.stopPropagation();
+    };
     $(".main_body").on("click.show_task_list", ".task_list", this.activateTaskList);
     $(".tasks_container").on("click.show_add_task_form", ".button_new_task", this.openAddTaskForm);
+    $(".add_task_form").on("click.show_add_task_form", ".add_task_form_input", this.preventPropagation);
 }
 
 $(document).ready(function(){
