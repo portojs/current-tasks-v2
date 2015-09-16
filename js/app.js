544114e26fb7f2a506ea7e9e9ebfc9b889a7177b
peter <peter.k.nets@gmail.com>
2015-09-16 14:12:28 +0300

    Introduce new variable

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
  * Created by Peter on 22.07.2015.
  */
 function TaskListLogic() {
+    var tsksContainer = $(".tasks_container");
     this.activateTaskList = function() {
         $(this).toggleClass("task_list_active");
         if ($(this).hasClass("task_list_active")) {
@@ -27,9 +28,9 @@ function TaskListLogic() {
         }
     };
     $(".main_body").on("click.show_task_list", ".task_list", this.activateTaskList);
-    $(".tasks_container").on("click.add_task_form", ".button_new_task", this.openAddTaskForm);
-    $(".add_task_form").on("click.add_task_form", ".add_task_form_input", this.preventPropagation);
-    $(".add_task_form").on("click.add_task_form", ".button_add_task", this.addTask)
+    tsksContainer.on("click.add_task_form", ".button_new_task", this.openAddTaskForm);
+    tsksContainer.on("click.add_task_form", ".add_task_form_input", this.preventPropagation);
+    tsksContainer.on("click.add_task_form", ".button_add_task", this.addTask)
 }
 
 $(document).ready(function(){
